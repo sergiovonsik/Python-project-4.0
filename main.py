d420fe760ab82ef5fb51953d9ca2e217404349d7
sergiovonsik <103293248+sergiovonsik@users.noreply.github.com>
2022-08-16 10:14:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,14 @@
 
 def result(x):
     x = x.replace("_", " ")
-    print('---------')
-    print(f'| {x[0]} {x[1]} {x[2]} |')
-    print(f'| {x[3]} {x[4]} {x[5]} |')
-    print(f'| {x[6]} {x[7]} {x[8]} |')
-    print('---------')
+    print('-------------')
+    print(f'| {x[0]} | {x[1]} | {x[2]} |')
+    print('-------------')
+    print(f'| {x[3]} | {x[4]} | {x[5]} |')
+    print('-------------')
+    print(f'| {x[6]} | {x[7]} | {x[8]} |')
+    print('-------------')
+
 
 
 # def to find where to put "X"
